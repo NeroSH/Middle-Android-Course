3dbbba55997fea3ae6130bfcbdc43082eecad342
NeroSH <mustafo.xon@gmail.com>
2020-07-27 12:57:58 +0300

    Iterable.dropLastUntil()

--- app/src/main/java/ru/skillbranch/kotlinexample/extensions/Iterable.kt
+++ app/src/main/java/ru/skillbranch/kotlinexample/extensions/Iterable.kt
@@ -1,3 +1,13 @@
 package ru.skillbranch.kotlinexample.extensions
 
-// TODO - make some functions
+inline fun <T> List<T>.dropLastUntil(predicate: (T) -> Boolean): List<T> {
+    if (!isEmpty()) {
+        val iterator = listIterator(size)
+        while (iterator.hasPrevious()) {
+            if (!predicate(iterator.previous())) {
+                return take(iterator.nextIndex() - 1)
+            }
+        }
+    }
+    return emptyList()
+}
